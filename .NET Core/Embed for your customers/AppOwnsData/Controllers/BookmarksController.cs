7771be1d2517340fc3a31bac006d7c28fb42c843
Adam Riddick <adam.riddick@isams.co.uk>
2021-06-15 17:04:45 +0100

    added report visual customisation & bookmarks

---
+++ .NET Core/Embed for your customers/AppOwnsData/Controllers/BookmarksController.cs
@@ -0,0 +1,22 @@
+﻿// ----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+// ----------------------------------------------------------------------------
+
+namespace AppOwnsData.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class BookmarksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Share()
+        {
+            return View();
+        }
+    }
+}
