cdef4f4e91911d95b80452ae41f8bacaf5b1852d
Adam Riddick <adam.riddick@isams.co.uk>
2021-06-16 13:01:52 +0100

    theming

---
+++ .NET Core/Embed for your customers/AppOwnsData/Controllers/StylesController.cs
@@ -0,0 +1,22 @@
+﻿// ----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+// ----------------------------------------------------------------------------
+
+namespace AppOwnsData.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class StylesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Share()
+        {
+            return View();
+        }
+    }
+}
